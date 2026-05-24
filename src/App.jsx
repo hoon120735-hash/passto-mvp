bd6ff4372fe7aaf62bb06321bb3079501c2b11e2
hoon120735-hash <hoon120735@gmail.com>
2026-05-24 16:13:09 +0900

    Fix import statement typo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-mport React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { supabase } from "./lib/supabase";
 import "./style.css";
 import {
